667c9c532e1e162763027f457e9b6950eb4f7cd9
alexandre.vachez <alexandre.vachez@edu.ece.fr>
2025-01-22 12:57:51 +0100

    First working version with Blynk

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,56 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
-// Define the pins that we will use
+#include <WiFi.h>
+#include <WiFiClient.h>
+ 
+// Blynk configuration
+#define BLYNK_TEMPLATE_ID "TMPL5vrQl_RXw"
+#define BLYNK_TEMPLATE_NAME "TP2 Groupe 5"
+#define BLYNK_AUTH_TOKEN "DMXKsX91hboZlGMApwbpMrNbc-uKvfYE"
+ 
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp32.h>
+ 
+// Définition des broches
 #define CAPTEUR 33
 #define LED 26
 #define DHTTYPE DHT11
-
+ 
+// WiFi credentials
+char ssid[] = "HelloWorld";
+char pass[] = "testtest";
+ 
+// Initialisation du capteur DHT
 DHT_Unified dht(CAPTEUR, DHTTYPE);
-
-// WiFi credentials go here
-// ...
-// ...
-// ...
-
+ 
+// Fonction appelée quand on reçoit une valeur sur V2 (LED)
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt(); 
+  Serial.print("Received value from Blynk: ");
+  Serial.println(pinValue);
+  digitalWrite(LED, pinValue);
+  delay(1000);
+}
+ 
 void setup() {
   // Setup pins
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
-
   // Begin serial communication
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(100);
-
-  // begin the Blynk session
-  // ...
-  // ...
-  // ...
-
+  // Connexion à Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Blynk.run();
+ 
+  // Force la mise à jour du pin virtuel V2 (LED)
+  Blynk.syncVirtual(V2);
   // Start listening to the DHT11
   dht.begin();
-
   sensors_event_t event;
-
+ 
   // Get temperature event and print its value
   float temp_measure = -999.0;
   dht.temperature().getEvent(&event);
@@ -44,8 +62,9 @@ void setup() {
     Serial.print(event.temperature);
     Serial.println(F("°C"));
     temp_measure = event.temperature;
+    Serial.print(temp_measure);
   }
-
+ 
   // Get humidity event and print its value.
   float relative_humidity_measure = -999.0;
   dht.humidity().getEvent(&event);
@@ -57,17 +76,19 @@ void setup() {
     Serial.println(F("%"));
     relative_humidity_measure = event.relative_humidity;
   }
-
-  // Send data to Blynk
-  // ...
-  // ...
-  // ...
-
+ 
+  // Send data to Blynk if measurements are valid
+  if (temp_measure != -999.0 && relative_humidity_measure != -999.0) {
+    Blynk.virtualWrite(V0, relative_humidity_measure);  // Envoi de l'humidité
+    Blynk.virtualWrite(V1, temp_measure);  // Envoi de la température
+    Blynk.run();  // Pour s'assurer que les données sont envoyées
+  }
+ 
   Serial.println("Going to sleep for 5 seconds...");
   delay(100);
   ESP.deepSleep(5e6);
 }
-
+ 
 void loop() {
   // Not needed anymore, the function is kept so PlatformIO does not complain.
 }
